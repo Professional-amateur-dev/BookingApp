476120cf45b320e4ee059e9a5ddb07f125633fa6
Professional-amateur-dev <dpetekpav@tvz.hr>
2020-08-08 16:13:38 +0200

    Add_Navbar

--- BookingApp.Web/src/app/app.module.ts
+++ BookingApp.Web/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookingsModule } from './bookings/bookings.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatListModule } from '@angular/material/list';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
@@ -18,10 +26,21 @@ import { BookingsModule } from './bookings/bookings.module';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
-    BookingsModule
+    BookingsModule,
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatListModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]

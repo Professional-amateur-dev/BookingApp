4c82f1dd5678b4572345e047c82dc4ac1c73cc8e
Professional-amateur-dev <dpetekpav@tvz.hr>
2020-08-10 15:30:58 +0200

    Fix_Bugs

--- BookingApp.Web/src/app/home-page/home-page.module.ts
+++ BookingApp.Web/src/app/home-page/home-page.module.ts
@@ -5,21 +5,29 @@ import { HomePageRoutingModule } from './home-page-routing.module';
 import { GalleryInfoComponent } from './gallery-info/gallery-info.component';
 import { VideoQuoteComponent } from './video-quote/video-quote.component';
 import { HomePageComponent } from './home-page.component';
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RoomsComponent } from './rooms/rooms.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
     GalleryInfoComponent,
     VideoQuoteComponent,
-    HomePageComponent
+    HomePageComponent,
+    RoomsComponent
   ],
   imports: [
     CommonModule,
     HomePageRoutingModule,
     FlexLayoutModule,
-    NgbModule
+    NgbModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   exports: [
     FlexLayoutModule
